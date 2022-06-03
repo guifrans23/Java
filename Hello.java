fd9deb59b5c754d8ab0780d2698f532a240d70e1
Guifrans <87093840+legends23@users.noreply.github.com>
2022-06-03 16:45:28 -0300

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,26 @@
+/**
+ * hello word 
+ * @author guifrans
+ **/
+package hello;
+
+import javax.swing.JOptionPane;
+
+/**
+ * classe principal- hello Word
+ * 
+ * @author guifrans
+ *
+ */
+
+public class Hello {
+	/**
+	 * metodo principal - hello word
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		//System.out.println("hello word");
+		JOptionPane.showMessageDialog(null,"hello Word");
+	}
+
+}

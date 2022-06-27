f6358c971a9bb53dce73a493fad416757cfeb4b3
Guifrans <87093840+legends23@users.noreply.github.com>
2022-06-27 15:20:01 -0300

    Add files via upload

--- stroggames/src/view/Principal.java
+++ stroggames/src/view/Principal.java
@@ -13,6 +13,7 @@ import javax.swing.JButton;
 import java.awt.Cursor;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.Color;
 
 public class Principal extends JFrame {
 
@@ -42,51 +43,93 @@ public class Principal extends JFrame {
 	 * Create the frame.
 	 */
 	public Principal() {
+		setResizable(false);
 		setIconImage(Toolkit.getDefaultToolkit().getImage(Principal.class.getResource("/img/logo.png")));
 		setTitle("stronggames");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 564, 391);
+		setBounds(100, 100, 647, 448);
 		contentPane = new JPanel();
+		contentPane.setToolTipText("Relatorio");
 		contentPane.setCursor(Cursor.getPredefinedCursor(Cursor.DEFAULT_CURSOR));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JLabel lblNewLabel = new JLabel("");
-		lblNewLabel.setIcon(new ImageIcon(Principal.class.getResource("/img/logo (2).png")));
-		lblNewLabel.setBounds(410, 197, 128, 144);
-		contentPane.add(lblNewLabel);
+		JButton btnNewButton_3 = new JButton("");
+		btnNewButton_3.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnNewButton_3.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		btnNewButton_3.setIcon(new ImageIcon(Principal.class.getResource("/img/user.png")));
+		btnNewButton_3.setToolTipText("Usuarios");
+		btnNewButton_3.setBounds(10, 11, 128, 128);
+		contentPane.add(btnNewButton_3);
+		
+		JButton btnNewButton_4 = new JButton("");
+		btnNewButton_4.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		btnNewButton_4.setIcon(new ImageIcon(Principal.class.getResource("/img/pagamento.png")));
+		btnNewButton_4.setToolTipText("Pagamento");
+		btnNewButton_4.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnNewButton_4.setBounds(10, 167, 128, 128);
+		contentPane.add(btnNewButton_4);
+		
+		JButton btnNewButton_5 = new JButton("");
+		btnNewButton_5.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		btnNewButton_5.setIcon(new ImageIcon(Principal.class.getResource("/img/Relatorio.png")));
+		btnNewButton_5.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnNewButton_5.setToolTipText("Relatorio");
+		btnNewButton_5.setBounds(164, 167, 128, 128);
+		contentPane.add(btnNewButton_5);
+		
+		JButton btnNewButton_6 = new JButton("");
+		btnNewButton_6.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		btnNewButton_6.setIcon(new ImageIcon(Principal.class.getResource("/img/produtos.png")));
+		btnNewButton_6.setToolTipText("Produtos");
+		btnNewButton_6.setBounds(326, 11, 128, 128);
+		contentPane.add(btnNewButton_6);
+		
+		JButton btnNewButton_7 = new JButton("");
+		btnNewButton_7.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		btnNewButton_7.setIcon(new ImageIcon(Principal.class.getResource("/img/fornecedores.png")));
+		btnNewButton_7.setToolTipText("Fornecedores");
+		btnNewButton_7.setBounds(164, 11, 128, 128);
+		contentPane.add(btnNewButton_7);
 		
 		JButton btnNewButton = new JButton("");
-		btnNewButton.setIcon(new ImageIcon(Principal.class.getResource("/img/fornecedor.png")));
 		btnNewButton.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
-		btnNewButton.setToolTipText("Fornecedores");
-		btnNewButton.setBounds(10, 11, 128, 128);
+		btnNewButton.setIcon(new ImageIcon(Principal.class.getResource("/img/clientes.png")));
+		btnNewButton.setToolTipText("Clientes");
+		btnNewButton.setBounds(477, 11, 128, 128);
 		contentPane.add(btnNewButton);
 		
 		JButton btnNewButton_1 = new JButton("");
 		btnNewButton_1.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		btnNewButton_1.setIcon(new ImageIcon(Principal.class.getResource("/img/configuracoes.png")));
 		btnNewButton_1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 			}
 		});
-		btnNewButton_1.setIcon(new ImageIcon(Principal.class.getResource("/img/clientes.png")));
-		btnNewButton_1.setToolTipText("clientes");
-		btnNewButton_1.setBounds(161, 11, 128, 128);
+		btnNewButton_1.setToolTipText("ferramentas");
+		btnNewButton_1.setBounds(326, 167, 128, 128);
 		contentPane.add(btnNewButton_1);
 		
-		JButton btnNewButton_3 = new JButton("");
-		btnNewButton_3.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
-		btnNewButton_3.setIcon(new ImageIcon(Principal.class.getResource("/img/sobre.png")));
-		btnNewButton_3.setToolTipText("sobre");
-		btnNewButton_3.setBounds(474, 11, 64, 64);
-		contentPane.add(btnNewButton_3);
+		JPanel panel = new JPanel();
+		panel.setBackground(Color.RED);
+		panel.setBounds(0, 364, 627, 50);
+		contentPane.add(panel);
 		
 		JButton btnNewButton_2 = new JButton("");
-		btnNewButton_2.setIcon(new ImageIcon(Principal.class.getResource("/img/produtos.png")));
-		btnNewButton_2.setToolTipText("produtos");
 		btnNewButton_2.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
-		btnNewButton_2.setBounds(10, 169, 128, 128);
+		btnNewButton_2.setIcon(new ImageIcon(Principal.class.getResource("/img/sobre (2).png")));
+		btnNewButton_2.setToolTipText("sobre");
+		btnNewButton_2.setBounds(477, 167, 128, 128);
 		contentPane.add(btnNewButton_2);
 	}
 }
